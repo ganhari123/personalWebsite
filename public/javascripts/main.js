b7abcc178a456ea0e57b0c75e19f6b0237e8ad94
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-09-28 21:26:29 -0500

    Added tab colors and panes for every tab

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,47 @@
 $(document).ready(function() {
   $('#con').click(function(){
     $('#contactMePanel').show(100);
+    $('#resume').hide(100);
+    $('#projects').hide(100);
+    $('#con').css('border-bottom', '4px solid #7f0000');
+    $('#res').css('border-bottom', 'none');
+    $('#proj').css('border-bottom', 'none');
   });
 
   $('#title').click(function(){
     $('#contactMePanel').hide(100);
+    $('#resume').hide(100);
+    $('#projects').hide(100);
+    $('#con').css('border-bottom', 'none');
+    $('#res').css('border-bottom', 'none');
+    $('#proj').css('border-bottom', 'none');
+  });
+
+
+  $("#subTit").click(function(){
+  	$('#contactMePanel').hide(100);
+  	$('#resume').hide(100);
+  	$('#projects').hide(100);
+    $('#con').css('border-bottom', 'none');
+    $('#res').css('border-bottom', 'none');
+    $('#proj').css('border-bottom', 'none');
+  });
+
+  $('#res').click(function(){
+  	$('#contactMePanel').hide(100);
+  	$('#resume').show(100);
+  	$('#projects').hide(100);
+  	$('#res').css('border-bottom', '4px solid #7f0000');
+  	$('#con').css('border-bottom', 'none');
+  	$('#proj').css('border-bottom', 'none');
+  });
+
+  $('#proj').click(function(){
+  	$('#contactMePanel').hide(100);
+  	$('#resume').hide(100);
+  	$('#projects').show(100);
+  	$('#proj').css('border-bottom', '4px solid #7f0000');
+  	$('#con').css('border-bottom', 'none');
+  	$('#res').css('border-bottom', 'none');
   });
 });
