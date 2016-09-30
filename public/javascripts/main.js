e68bf71ae02fd3c128c5d44cc3b01edc6fe4687d
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-09-30 15:44:12 -0500

    Added panes to projects

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,35 +1,39 @@
 $(document).ready(function() {
   $('#con').click(function(){
-    $('#contactMePanel').show(100);
-    $('#resume').hide(100);
-    $('#projects').hide(100);
+    $('#contactMePanel').show(2);
+    $('#resume').hide(2);
+    $('#projects').hide(2);
+    $('#menu').css('opacity', '0.72');
     $('#con').css('border-bottom', '4px solid #7f0000');
     $('#res').css('border-bottom', 'none');
     $('#proj').css('border-bottom', 'none');
   });
 
   $('#res').click(function(){
-  	$('#contactMePanel').hide(100);
-  	$('#resume').show(100);
-  	$('#projects').hide(100);
+  	$('#contactMePanel').hide(2);
+  	$('#resume').show(2);
+  	$('#projects').hide(2);
+  	$('#menu').css('opacity', '0.72');
   	$('#res').css('border-bottom', '4px solid #7f0000');
   	$('#con').css('border-bottom', 'none');
   	$('#proj').css('border-bottom', 'none');
   });
 
   $('#proj').click(function(){
-  	$('#contactMePanel').hide(100);
-  	$('#resume').hide(100);
-  	$('#projects').show(100);
+  	$('#contactMePanel').hide(2);
+  	$('#resume').hide(2);
+  	$('#projects').show(2);
   	$('#proj').css('border-bottom', '4px solid #7f0000');
+  	$('#menu').css('opacity', '1');
   	$('#con').css('border-bottom', 'none');
   	$('#res').css('border-bottom', 'none');
   });
 
   $('#dummyPanel').click(function(){
-  	$('#contactMePanel').hide(100);
-  	$('#resume').hide(100);
-  	$('#projects').hide(100);
+  	$('#contactMePanel').hide(2);
+  	$('#resume').hide(2);
+  	$('#projects').hide(2);
+  	$('#menu').css('opacity', '0.72');
   	$('#proj').css('border-bottom', 'none');
   	$('#con').css('border-bottom', 'none');
   	$('#res').css('border-bottom', 'none');
