572226122b8164bcec4beabc9303a6fb40f76210
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-10-05 09:57:21 -0500

    Added projects that I have done thus far

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -23,14 +23,25 @@ $(document).ready(function() {
 
   	$.get('/projects', function(data, status){
   		console.log(data);
+  		$('.row > .col-md-5 > .panel.panel-default > .panel-heading').each(function(index, value){
+  			$(this).text(data.projects[index].name);
+  		});
+
+  		$('.row > .col-md-5 > .panel.panel-default > .panel-body').each(function(index, value){
+  			$(this).text(data.projects[index].description);
+  		});
+  		$('.row > .col-md-10 > .panel.panel-default > .panel-body').text(data.projects[4].description);
+		$('.row > .col-md-10 > .panel.panel-default > .panel-heading').text(data.projects[4].name);
+
+  		$('#contactMePanel').hide(2);
+	  	$('#resume').hide(2);
+	  	$('#projects').show(2);
+	  	$('#proj').css('border-bottom', '4px solid #7f0000');
+	  	$('#menu').css('opacity', '1');
+	  	$('#con').css('border-bottom', 'none');
+	  	$('#res').css('border-bottom', 'none');
   	});
-  	$('#contactMePanel').hide(2);
-  	$('#resume').hide(2);
-  	$('#projects').show(2);
-  	$('#proj').css('border-bottom', '4px solid #7f0000');
-  	$('#menu').css('opacity', '1');
-  	$('#con').css('border-bottom', 'none');
-  	$('#res').css('border-bottom', 'none');
+  	
   });
 
   $('#dummyPanel').click(function(){
